23a26278afe50db75c282a80a5589889c0cd1512
ermtedla <149193108+ermtedla@users.noreply.github.com>
2023-10-27 17:01:16 -0400

    Chain.java

--- src/Chain.java
+++ src/Chain.java
@@ -1,4 +1,18 @@
 
-public class Chain {
+public class Chain extends Item {
 
+	public Chain() {
+		super("chain");
+	}
+	
+	public void use() {
+		if (Game.getCurrentRoom().getName().equals("lobby")) {
+			System.out.println("Check for any goo");
+			Room lounge = Game.getCurrentRoom().getExit('e');
+			lounge.setLocked(false);
+		} else {
+			System.out.println("No goo detected");
+		}
+	}
+	
 }
