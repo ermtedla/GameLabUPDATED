7a6718e639bba720c7cd1b8c27556f81205aa83a
ermtedla <149193108+ermtedla@users.noreply.github.com>
2023-10-27 17:00:47 -0400

    Goo.java

--- src/Goo.java
+++ src/Goo.java
@@ -1,4 +1,18 @@
 
-public class Goo {
+public class Goo extends Item {
 
+	public Goo() {
+		super("goo");
+	}
+	
+	public void use() {
+		if (Game.getCurrentRoom().getName().equals("lobby")) {
+			System.out.println("Look out for goo!");
+			Room lounge = Game.getCurrentRoom().getExit('e');
+			lounge.setLocked(false);
+		} else {
+			System.out.println("There is no goo");
+		}
+	}
+	
 }
