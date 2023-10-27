088a663bdf8d2a092826584fdaaf8f12232b30f0
ermtedla <149193108+ermtedla@users.noreply.github.com>
2023-10-27 17:00:16 -0400

    Shower.java

--- src/Shower.java
+++ src/Shower.java
@@ -1,4 +1,18 @@
 
-public class Shower {
+public class Shower extends Item {
 
+	public Shower() {
+		super("shower");
+	}
+	
+	public void use() {
+		if (Game.getCurrentRoom().getName().equals("lobby")) {
+			System.out.println("You are entering the shower, look out for goo!");
+			Room lounge = Game.getCurrentRoom().getExit('e');
+			lounge.setLocked(false);
+		} else {
+			System.out.println("There is no goo")l;
+		}
+	}
+	
 }
